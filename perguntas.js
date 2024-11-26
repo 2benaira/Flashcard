145ac2152e106f26be4735cdbf64d32673964d66
2benaira <00001115337464sp@al.educacao.sp.gov.br>
2024-11-26 17:37:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Livro A culpa é das Estrela',
+    'Qual o medo de Hazel e como Augustus a ajuda?',
+    'Hazel teme ser esquecida, e Augustus ajuda a ver que sua vida tem significado, mesmo sendo curta'
+)
+
+criaCartao(
+    'Livro Eleanor & Park',
+    'Quais são os maiores desafios de Eleanor e Park?',
+    'Eleanor enfrenta problemas em casa e bullying, enquanto Park lida com expectativas familiares'
+)
+
+criaCartao(
+    'Livro Anna e o Beijo Francês',
+    'O que  o livro ensina sobre "casa"?',
+    '"Casa" é onde encontramos pertencimento, não apenas um lugar físico'
+)
+
+criaCartao(
+    'Livro Depois de Você',
+    'O que o livro diz sobre segundas chances?',
+    'O livro mostra como Louisa tem uma segunda chance no amor e na vida'
+)
